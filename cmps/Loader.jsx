bc1bea1174c88c6866453e471a144f6fa5688d66
Omri Madar <gunrh390539@gmail.com>
2021-05-03 20:12:34 +0300

    finalize loader cmp

---
+++ cmps/Loader.jsx
@@ -0,0 +1,7 @@
+export function Loader() {
+    return (
+        <section className="container loader-container flex justify-center align-center">
+            <img src="../assets/img/tail-spin.svg" />
+        </section>
+    )
+}
