d4a9ebfd58b06f6a66c562e111d26c94fe55f4d7
Omri Madar <gunrh390539@gmail.com>
2021-05-03 16:05:18 +0300

    check check

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -3,7 +3,7 @@ export function Home() {
 
     <section className="container">
       <h2>Welcome to our home page</h2>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est laudantium earum veniam in aspernatur nam fuga tempore! Doloribus, possimus nesciunt.</p>
+      <p>!!!!!!!!!!!Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est laudantium earum veniam in aspernatur nam fuga tempore! Doloribus, possimus nesciunt.</p>
 
     </section>
   )
