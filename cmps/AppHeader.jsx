0c19a7619aba803d3b385365bb38fe4590a40c0b
Omri Madar <gunrh390539@gmail.com>
2021-05-03 19:46:48 +0300

    loader update

--- cmps/AppHeader.jsx
+++ cmps/AppHeader.jsx
@@ -5,7 +5,7 @@ export function AppHeader() {
   return (
     <nav className="app-header container">
       <ul className="clean-list flex space-between align-center">
-        <li><NavLink to="/">Home</NavLink></li>
+        <li><NavLink to="/" exact={true}>Home</NavLink></li>
         <li><NavLink to="/about">About</NavLink></li>
         <li><NavLink to="/mail">Email</NavLink></li>
         <li><NavLink to="/keep">Keeps</NavLink></li>
