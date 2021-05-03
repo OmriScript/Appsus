6bd6a27227a9669ed44283ce7020bbedbdd48bd8
idanlevi507 <idanlevi15@gmail.com>
2021-05-03 20:20:17 +0300

    basic

--- cmps/Footer.jsx
+++ cmps/Footer.jsx
@@ -3,7 +3,7 @@ export function Footer() {
 
   return (
     <section className="app-footer container flex justify-center align-center">
-      <p>©I&O coffee-rights 2021.</p>
+      <p>© I&O coffee-rights 2021.</p>
     </section>
   )
 }
--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,7 +1,7 @@
 export function Home() {
   return (
 
-    <section className="container">
+    <section className="home container flex column  align-center">
       <h2>Welcome to our home page</h2>
       <p>!!!!!!!!!!!Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est laudantium earum veniam in aspernatur nam fuga tempore! Doloribus, possimus nesciunt.</p>
 
