76bd7fac1d5937a6b6031487e07b792ae730ad70
idanlevi507 <idanlevi15@gmail.com>
2021-05-04 10:16:38 +0300

    first comp

--- pages/MailApp.jsx
+++ pages/MailApp.jsx
@@ -1,7 +1,30 @@
-export function MailApp() {
+import {mailService} from '../services/mail-service.js'
+
+export class MailApp extends React.Component {
+
+  state = {
+
+  }
+
+  componentDidMount () {
+    this.getInMAils()
+  }
+
+
+getInMAils = () => {
+  mailService.getInMails()
+  .then(inMails => {
+    console.log(inMails);
+  })
+}
+
+
+render() {
+  
   return (
     <section className="container">
       <h2>MailApp</h2>
     </section>
   )
+}
 }
