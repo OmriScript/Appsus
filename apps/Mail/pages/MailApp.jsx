d852a6011869851e270df9e9c07dcd4e0119a50e
idanlevi507 <idanlevi15@gmail.com>
2021-05-04 18:17:09 +0300

    basic page css

--- apps/Mail/pages/MailApp.jsx
+++ apps/Mail/pages/MailApp.jsx
@@ -1,4 +1,4 @@
-import {mailService} from '../services/mail-service.js'
+import { mailService } from '../services/mail-service.js'
 
 export class MailApp extends React.Component {
 
@@ -6,25 +6,83 @@ export class MailApp extends React.Component {
 
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.getInMAils()
   }
 
 
-getInMAils = () => {
-  mailService.getInMails()
-  .then(inMails => {
-    console.log(inMails);
-  })
-}
+  getInMAils = () => {
+    mailService.getInMails()
+      .then(inMails => {
+        console.log(inMails);
+      })
+  }
 
 
-render() {
-  
-  return (
-    <section className="mail-app">
-      <h2>MailApp</h2>
-    </section>
-  )
-}
+  render() {
+
+    return (
+      <section className="mail-app flex">
+
+        <section className="mail-side-bar flex column ">
+          <button className="mail-compose-btn padding">Compose</button>
+          <ul>
+            <li><a className="mail-inbox-btn"> Inbox </a></li>
+            <li><a className="mail-starred-btn">Starred </a></li>
+            <li><a className="mail-sent-btn">Sent Mail </a></li>
+            <li><a className="mail-drafts-btn">Drafts </a></li>
+            <li><a className="mail-drafts-btn">[------18%------] </a></li>
+          </ul>
+
+        </section>
+
+        <section className="mail-main-container flex column">
+
+          <section className="mail-search-bar">
+            <input type="text" className="mail-search-input" placeholder="Search-mail" />
+            <select className="padding">
+              <option>All</option>
+              <option>Read</option>
+              <option>Unread</option>
+            </select>
+          </section>
+
+          <section className="mail-mails-container " >
+
+            <div className="mail-mail-preview flex space-between ">
+              <i>*</i>
+              <div className="name">Name</div>
+              <div className="mail-subject">Subject</div>
+              <div className="mail-message">Message</div>
+              <div className="mail-time">Time</div>
+
+            </div>
+            <div className="mail-mail-preview flex space-between ">
+              <i>*</i>
+              <div className="name">Name</div>
+              <div className="mail-subject">Subject</div>
+              <div className="mail-message">Message</div>
+              <div className="mail-time">Time</div>
+
+            </div>
+            <div className="mail-mail-preview flex space-between ">
+              <i>*</i>
+              <div className="name">Name</div>
+              <div className="mail-subject">Subject</div>
+              <div className="mail-message">Message</div>
+              <div className="mail-time">Time</div>
+
+            </div>
+
+          </section>
+
+
+        </section>
+
+
+      </section>
+
+
+    )
+  }
 }
