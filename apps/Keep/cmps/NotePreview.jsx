e08df60252e72acf798d2c0212a12d4cce0b4f13
Omri Madar <gunrh390539@gmail.com>
2021-05-05 21:05:44 +0300

    video input fixed

--- apps/Keep/cmps/NotePreview.jsx
+++ apps/Keep/cmps/NotePreview.jsx
@@ -34,20 +34,19 @@ export class NotePreview extends React.Component {
 
         return (
             <div className="note-preview" >
-                {/* {note.info.imgUrl &&
-                 <p>{note.info.imgUrl}</p>} */}
-
-                {note.info.imgUrl && <img src={note.info.imgUrl}></img>}
-
-
-
-                {/* {
-                    note.info.txt && <h1 className="note-preview-header" contentEditable="true" suppressContentEditableWarning={true} onChange={this.handleTitleChange}> {note.info.txt}
-                    </h1>
-                } */}
                 < section className="note-preview-details" >
-                    {< p contentEditable="true" suppressContentEditableWarning={true} > Edit me</p>}
-                </section >
+
+                    {note.info.txt && <p contentEditable="true" suppressContentEditableWarning={true}>{note.info.txt}</p>}
+
+                    {note.info.imgUrl && <img src={note.info.imgUrl} onError={noteService.handleImgSrcError(this)}></img>}
+
+                    {note.info.videoUrl && <iframe width="230" height="150" src={note.info.videoUrl} frameBorder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowFullScreen
+                        title="Embedded youtube" ></iframe>}
+
+                </section>
+
                 <section className="note-preview-control-panel flex justify-end align-end">
                     <div className="note-preview-btn-container ">
                         <button>
