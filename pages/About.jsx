fb8d492253b90cccd5fda53016afd7a0b3b162c9
Omri Madar <gunrh390539@gmail.com>
2021-05-08 20:27:59 +0300

    some css

--- pages/About.jsx
+++ pages/About.jsx
@@ -27,12 +27,11 @@ function AboutVision() {
 }
 
 export function About() {
-  return <section className="about-container  container flex column justify-center ">
+  return <section className="about-container  container flex column flex-start align-center">
     <h2>About Us</h2>
-    <p>We joined forces for building this app and learning React together.
-    Build in 96 hours.
+    <p>After only 9 academic hours of learning react from scratch,
     </p>
-
+    <p>We joined forces and build this useful app in 96 hours as part of Coding Academy course.</p>
     <nav>
       <NavLink to="/about/team">Team</NavLink>
       <NavLink to="/about/vision">Vision</NavLink>
